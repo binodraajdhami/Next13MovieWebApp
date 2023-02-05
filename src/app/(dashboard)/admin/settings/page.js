dc35049789e373689b956da1ad439cdda643c220
Binod Raj Dhami <binodraazdhami@gmail.com>
2023-02-05 23:33:06 +0545

    disable eslint

---
+++ src/app/(dashboard)/admin/settings/page.js
@@ -0,0 +1,19 @@
+export default function AdminDashboardSetting() {
+    return (
+        <section class="admin-dashboard">
+            <div class="container-fluid">
+                <div class="admin-dashboard-header">
+                    <h2>Admin Dashboard Settings</h2>
+                </div>
+                <div className="admin-dashboard-body">
+                    <div class="admin-dashboard-sidebar">
+                        <h2>Side Menu</h2>
+                    </div>
+                    <div class="admin-dashboard-main">
+                        <h2>Dashboard</h2>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
