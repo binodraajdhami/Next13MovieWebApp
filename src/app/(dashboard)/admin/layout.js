18838d7a75a7aae131e5d2d59cd8d225b7e152e7
Binod Raj Dhami <binodraazdhami@gmail.com>
2023-02-06 14:23:10 +0545

    start making dashboard

--- src/app/(dashboard)/admin/layout.js
+++ src/app/(dashboard)/admin/layout.js
@@ -2,7 +2,22 @@
 export default function AdminLayout({ children }) {
     return (
         <>
-            {children}
+            <section class="admin-dashboard">
+                <div class="container-fluid">
+                    <div class="admin-dashboard-header">
+                        <h2>Admin Dashboard</h2>
+                    </div>
+                    <div className="admin-dashboard-body">
+                        <div class="admin-dashboard-sidebar">
+                            <h2>Side Menu</h2>
+                        </div>
+                        <div class="admin-dashboard-main">
+                            {children}
+                        </div>
+                    </div>
+                </div>
+            </section>
+
         </>
     );
 }
