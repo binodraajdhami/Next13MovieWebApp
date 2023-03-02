2dca5f52e1aec4bfd0ec52d2b0e68aabd28bfcc0
Binod Raj Dhami <binodraazdhami@gmail.com>
2023-03-02 17:17:29 +0545

    something went wrong

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,3 @@
-"use client";
-import { useEffect } from 'react';
-import { usePathname } from 'next/navigation';
-
 import './styles/globals.css';
 import "./styles/bootstrap.css";
 import "slick-carousel/slick/slick.css";
@@ -12,14 +8,13 @@ import Footer from './components/Footer';
 import GoToTop from "./components/GoToTop";
 
 export default function RootLayout({ children }) {
-    const pathname = usePathname();
     return (
         <html lang="en">
             <head />
             <body>
-                {pathname.startsWith('/admin/') ? <></> : <Header />}
+                <Header />
                 {children}
-                {pathname.startsWith('/admin/') ? <></> : <Footer />}
+                <Footer />
                 <GoToTop />
             </body>
         </html>
